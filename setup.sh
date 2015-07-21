34905dd26a40c41e2d454cc9218df1d2046b9dd2
Jack Wearden <jack@jackwearden.co.uk>
2015-07-21 12:54:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 wget -O - http://debian.neo4j.org/neotechnology.gpg.key | apt-key add - 
 echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
 apt-get update
-apt-get install openjdk-7-jre
-apt-get install -y neo4j
+apt-get install -y openjdk-7-jre neo4j
 sed -i'' 's/#org.neo4j.server.webserver.address/org.neo4j.server.webserver.address/' /etc/neo4j/neo4j-server.properties
 service neo4j-service restart
