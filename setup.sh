983d4ef2cb7c5299c6ada85f70e5d7fe6fa75a79
Jack Wearden <jack@jackwearden.co.uk>
2015-07-21 21:53:14 +0100

    Set initial password

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ apt-get update
 apt-get install -y openjdk-7-jre neo4j
 sed -i'' 's/#org.neo4j.server.webserver.address/org.neo4j.server.webserver.address/' /etc/neo4j/neo4j-server.properties
 service neo4j-service restart
+curl -X POST -H 'Accept: application/json; charset=UTF-8' -H 'Authorization: Basic bmVvNGo6bmVvNGo=' -H 'Content-Type: application/json' --data '{"password" : "vagrant"}' http://localhost:7474/user/neo4j/password
